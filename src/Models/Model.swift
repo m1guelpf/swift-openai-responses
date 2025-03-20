0cd87b19a0e6fe70ca21b61ae6b64b0520496dbd
Miguel Piedrafita <github@miguel.build>
2025-03-20 16:37:45 -0700

    Better `Model.init`

--- src/Models/Model.swift
+++ src/Models/Model.swift
@@ -14,30 +14,9 @@ public enum Model: Equatable, Hashable, Sendable {
 	case gpt3_5Turbo
 	case computerUsePreview
 	case other(String)
-}
 
-extension Model: Codable {
-	public func encode(to encoder: any Encoder) throws {
-		switch self {
-			case .o1: try "o1".encode(to: encoder)
-			case .gpt4: try "gpt-4".encode(to: encoder)
-			case .o1Pro: try "o1-pro".encode(to: encoder)
-			case .gpt4o: try "gpt-4o".encode(to: encoder)
-			case .o1Mini: try "o1-mini".encode(to: encoder)
-			case .o3Mini: try "o3-mini".encode(to: encoder)
-			case .gpt4oMini: try "gpt-4o-mini".encode(to: encoder)
-			case .gpt4Turbo: try "gpt-4o-turbo".encode(to: encoder)
-			case .gpt3_5Turbo: try "gpt-3.5-turbo".encode(to: encoder)
-			case .gpt4_5Preview: try "gpt-4.5-preview".encode(to: encoder)
-			case .computerUsePreview: try "computer-use-preview".encode(to: encoder)
-			case let .other(value): try value.encode(to: encoder)
-		}
-	}
-
-	public init(from decoder: any Decoder) throws {
-		let value = try String(from: decoder)
-
-		switch value {
+	public func init(_ model: String) throws {
+		switch model {
 			case "o1": self = .o1
 			case "gpt-4": self = .gpt4
 			case "gpt-4o": self = .gpt4o
@@ -49,7 +28,30 @@ extension Model: Codable {
 			case "gpt-3.5-turbo": self = .gpt3_5Turbo
 			case "gpt-4.5-preview": self = .gpt4_5Preview
 			case "computer-use-preview": self = .computerUsePreview
-			default: self = .other(value)
+			default: self = .other(model)
 		}
+	}
+}
+
+extension Model: Codable {
+	public func encode(to encoder: any Encoder) throws {
+		switch self {
+			case .o1: try "o1".encode(to: encoder)
+			case .gpt4: try "gpt-4".encode(to: encoder)
+			case .o1Pro: try "o1-pro".encode(to: encoder)
+			case .gpt4o: try "gpt-4o".encode(to: encoder)
+			case .o1Mini: try "o1-mini".encode(to: encoder)
+			case .o3Mini: try "o3-mini".encode(to: encoder)
+			case let .other(value): try value.encode(to: encoder)
+			case .gpt4oMini: try "gpt-4o-mini".encode(to: encoder)
+			case .gpt4Turbo: try "gpt-4o-turbo".encode(to: encoder)
+			case .gpt3_5Turbo: try "gpt-3.5-turbo".encode(to: encoder)
+			case .gpt4_5Preview: try "gpt-4.5-preview".encode(to: encoder)
+			case .computerUsePreview: try "computer-use-preview".encode(to: encoder)
+		}
+	}
+
+	public init(from decoder: any Decoder) throws {
+		try init(String(from: decoder))
 	}
 }
